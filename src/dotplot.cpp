621e5d4b6827e0a146eb11b8e4310696a2952b86
Ryan <rlorigro@ucsc.edu>
2021-04-11 12:59:28 -1000

    Dotplot exe using SVG plotter and mummer

--- src/dotplot.cpp
+++ src/dotplot.cpp
@@ -1,4 +1,5 @@
 #include "dotplot.hpp"
+#include "SvgPlot.hpp"
 #include "Plot.hpp"
 
 #include "mummer/sparseSA.hpp"
@@ -12,24 +13,27 @@ void generate_dotplot(
         path& output_directory){
 
     path plot_path = output_directory / (name + "_dotplot.svg");
-    Plot plot(plot_path, 800, 800, 18, 2);
+    SvgPlot plot(plot_path, 800, 800, 0, ref_sequence.size(), 0, ref_sequence.size());
 
-    vector <array <int64_t,2> > coords;
+    vector <array <int64_t,4> > coords;
 
     auto matcher = mummer::mummer::sparseSA::create_auto(ref_sequence.c_str(), ref_sequence.size(), 0, true);
 
     vector<mummer::mummer::match_t> mams;
 
+    string type = "rect";
+    string color = "#8115A3";
+
     matcher.findMAM_each(query_sequence, min_length, false, [&](const mummer::mummer::match_t& match){
         mams.emplace_back(match);
 
-        for (int64_t i=0; i<match.len; i+=50){
-            array <int64_t,2> coord = {match.ref + i, match.query + i};
-
-            coords.emplace_back(coord);
+        for (size_t i=0; i<match.len; i+=50) {
+            plot.add_point(
+                    match.ref + i,
+                    match.query + i,
+                    type,
+                    ref_sequence.size()/1000,
+                    color);
         }
     });
-
-    plot.add_points(coords, "", 0.1, "");
-    plot.generate();
 }
