251a502dd2e60a3be5c06a095766af29dd2d2836
Ryan <rlorigro@ucsc.edu>
2021-04-14 11:47:58 -1000

    Add runtime test for mummer

---
+++ src/test/test_mummer_runtime.cpp
@@ -0,0 +1,124 @@
+#include "FastqIterator.hpp"
+
+using overlap_analysis::FastqElement;
+using overlap_analysis::FastqIterator;
+
+#include <chrono>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using std::string;
+using std::vector;
+using std::cerr;
+
+#include "boost/program_options.hpp"
+#include "boost/bimap.hpp"
+
+using boost::program_options::options_description;
+using boost::program_options::variables_map;
+using boost::program_options::bool_switch;
+using boost::program_options::value;
+
+#include "mummer/sparseSA.hpp"
+
+using mummer::mummer::match_t;
+
+
+size_t compute_all_vs_all(vector <FastqElement>& sequences, uint32_t min_length){
+    size_t n_comparisons = 0;
+
+    for (const auto& s: sequences) {
+        auto matcher = mummer::mummer::sparseSA::create_auto(s.sequence.c_str(), s.sequence.size(), 0, true);
+
+        vector<match_t> matches;
+
+        for (const auto& s2: sequences) {
+            if (s.name == s2.name) {
+                continue;
+            }
+
+            matcher.findMAM_each(s2.sequence, min_length, false, [&](const match_t& match) {
+                matches.emplace_back(match);
+            });
+
+            n_comparisons++;
+        }
+    }
+
+    return n_comparisons;
+}
+
+
+void test(path fastq_path){
+    // Shortest match length counted by Mummer
+    uint32_t min_length = 12;
+
+    // How many times to loop over entire dataset
+    size_t n_trials = 10;
+
+    // Pre-load all the sequences into memory using a vector
+    vector <FastqElement> sequences;
+    FastqElement element;
+
+    FastqIterator fastq_iterator(fastq_path);
+
+    cerr << "Loading sequences..." << '\n';
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+
+    while (fastq_iterator.next_fastq_element(element)){
+        sequences.emplace_back(element);
+        cerr << "Read '" << element.name << "' = " << element.sequence.size() << " bp " <<'\n';
+    }
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+
+    cerr << "Completed in: " << elapsed.count() << " milliseconds" << '\n';
+    cerr << '\n';
+
+    cerr << "Computing exact matches for " << sequences.size() << " sequences (all vs all) with " << n_trials << " repeat trials..\n";
+
+    auto t3 = std::chrono::high_resolution_clock::now();
+
+    size_t total_comparisons = 0;
+    for (size_t i=0; i<n_trials; i++) {
+        total_comparisons += compute_all_vs_all(sequences, min_length);
+    }
+
+    auto t4 = std::chrono::high_resolution_clock::now();
+    auto elapsed2 = std::chrono::duration_cast<std::chrono::milliseconds>(t4 - t3);
+    cerr << "Completed in: " << elapsed2.count() << " milliseconds" << '\n';
+    cerr << "Total comparisons: " << total_comparisons << '\n';
+}
+
+
+int main(int argc, char* argv[]){
+    path fastq_path;
+    path output_directory;
+
+    options_description options("Arguments:");
+
+    options.add_options()
+            ("fastq,i",
+             value<path>(&fastq_path)
+                     ->required(),
+             "File path of PAF file containing alignments to some reference")
+            ;
+
+    variables_map vm;
+    store(parse_command_line(argc, argv, options), vm);
+
+    // If help was specified, or no arguments given, provide help
+    if (vm.count("help") || argc == 1) {
+        cerr << options << "\n";
+        return 0;
+    }
+    notify(vm);
+
+    test(fastq_path);
+
+    return 0;
+}
+
